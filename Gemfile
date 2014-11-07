4db580f34f0ab9749b3a8f3b211003a01b12d8a7
Sambath Prum <psb.sambath@gmail.com>
2014-11-07 14:42:45 -0600

    add authentication

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ group :test do
   gem 'spork-rails'
 end
 
+gem 'bootstrap-sass', '~>3.1.1'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   mount_griddler
 
+  root to: "welcome#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
